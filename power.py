4a6d4664ff6f53033191e63ab3a0b0d0548de5c5
priyatharshini23 <52314722+priyatharshini23@users.noreply.github.com>
2019-07-08 11:38:32 +0530

    Update and rename README.md to power.py

---
+++ power.py
@@ -0,0 +1,9 @@
+# 2
+num=int(input("Enter the positive integer:"))
+exponent=int(input("Enter exponent value:"))
+power=1
+i=1
+while(i<=exponent):
+power=power*num
+i=i+1
+print("The Result of{0}power{1}={2}".format(num,exponent,power)
