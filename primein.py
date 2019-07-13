7c96517be06839a443aafa6f6326471aae11d299
priyatharshini23 <52314722+priyatharshini23@users.noreply.github.com>
2019-07-13 19:24:05 +0530

    Update primein.py

--- primein.py
+++ primein.py
@@ -1,9 +1,9 @@
 lower=int(input("Enter lower range:")
 upper=int(input("Enter upper range:")
-for number in range(lower,upper+1):
-   if number>1:
-       for i in range(2,number):
-           if (number%i)==0:
+for num in range(lower,upper+1):
+   if num>1:
+       for i in range(2,num):
+           if (num%i)==0:
                break
        else:
-           print(number)
+           print(num)
