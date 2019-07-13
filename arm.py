69c8982b8edaa2437fdcae3493f93d5df687690a
priyatharshini23 <52314722+priyatharshini23@users.noreply.github.com>
2019-07-13 19:41:14 +0530

    Create arm.py

---
+++ arm.py
@@ -0,0 +1,7 @@
+num=int(input("Enter any number:")
+p=list(map(int,str(n)))
+r=list(map(lambda x:x**3,a))
+if(sum(r)=n):
+    print("The number is an armstrong number")
+else:
+    print("the number is not an armstrong number")
