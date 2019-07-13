1c91a09fc450300a650274a30cb3cfd981999802
priyatharshini23 <52314722+priyatharshini23@users.noreply.github.com>
2019-07-13 20:27:00 +0530

    Update armstrong.py

--- armstrong.py
+++ armstrong.py
@@ -1,7 +1,7 @@
 num=int(input("Enter any number:")
-p=list(map(int,str(n)))
-r=list(map(lambda x:x**3,a))
-if(sum(r)=n):
+p=list(map(int,str(num)))
+r=list(map(lambda x:x**3,p))
+if(sum(r)=num):
     print("Yes")
 else:
     print("no")
