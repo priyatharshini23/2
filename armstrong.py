ee423ca45a7904c3344aec5b008e2b87e4b60b46
priyatharshini23 <52314722+priyatharshini23@users.noreply.github.com>
2019-07-13 19:53:29 +0530

    Update armstrong.py

--- armstrong.py
+++ armstrong.py
@@ -2,6 +2,6 @@ num=int(input("Enter any number:")
 p=list(map(int,str(n)))
 r=list(map(lambda x:x**3,a))
 if(sum(r)=n):
-    print("The number is an armstrong number")
+    print("Yes")
 else:
-    print("the number is not an armstrong number")
+    print("no")
